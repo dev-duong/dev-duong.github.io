b09d3642bf33c356007c224ac4978ea0450109fc
Dev-Duong <bren.dandxyz@gmail.com>
2025-04-23 13:14:49 +1000

    cleaning

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
+const autoprefixer = require("autoprefixer");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [autoprefixer],
 };
