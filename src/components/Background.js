c3732819cab468c3144ace1c38491fe3f4f7fcc7
Dev-Duong <bren.dandxyz@gmail.com>
2025-04-25 08:46:17 +1000

    Removed Hero and added background.js

---
+++ src/components/Background.js
@@ -0,0 +1,17 @@
+import noise from "../assets/noise.png";
+
+const Background = () => {
+  return (
+    <div className="absolute inset-0 -z-10">
+      {/* Background image */}
+      <div
+        className="w-full h-full bg-cover bg-center"
+        style={{ backgroundImage: `url(${noise})` }}
+      />
+      {/* Overlay */}
+      <div className="absolute inset-0 bg-[#323232] opacity-95" />
+    </div>
+  );
+};
+
+export default Background;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Hero from "./components/Hero";
+import Background from "./components/Background";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Hero />
+    <Background />
   </React.StrictMode>
 );
