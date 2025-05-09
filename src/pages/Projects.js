5ef795db1345cfb6dfc3dba2cabdb2fc2b795226
Dev-Duong <bren.dandxyz@gmail.com>
2025-05-09 16:53:56 +1000

    Began projects page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,6 +1,9 @@
 // UI
 import SocialLinks from "../components/ui/SocialLinks";
 
+// Layout
+import Divider from "../components/layout/Divider";
+
 // Animations
 import PageWrapper from "../components/layout/PageWrapper";
 
@@ -9,7 +12,11 @@ const Projects = () => {
   return (
     <main className="pt-20 flex flex-col items-center">
       <PageWrapper>
-        <div>Project Page</div>
+        <section>
+          <article>
+            <Divider title="Projects" />
+          </article>
+        </section>
       </PageWrapper>
       <footer className="fixed bottom-0 right-0">
         <PageWrapper>
