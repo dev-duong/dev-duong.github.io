b7829727a3e4bfc01538fcdf49576e73af1d8d41
Dat <devduong.dat@gmail.com>
2026-02-05 21:03:01 +1000

    Update Copyright.js

--- src/components/universal/layout/Copyright.js
+++ src/components/universal/layout/Copyright.js
@@ -1,7 +1,7 @@
 const VerticalCopyright = () => {
   return (
     <div className="fixed text-sm tracking-widest text-gray-400 transform rotate-90 translate-x-1/2 -translate-y-1/2 pointer-events-none top-1/2 right-4">
-      © 6 DevDuong. All rights reserved.
+      © 2026 DevDuong. All rights reserved.
     </div>
   );
 };
